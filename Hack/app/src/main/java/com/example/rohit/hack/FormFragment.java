a4bb7a58d21732ad7c6e024ba060d8514d98a24c
Sainath Prasanna <sainathprasanna@gmail.com>
2018-11-28 14:50:20 +0530

    Location services added to forms address can now be automatically filled using location services

--- Hack/app/src/main/java/com/example/rohit/hack/FormFragment.java
+++ Hack/app/src/main/java/com/example/rohit/hack/FormFragment.java
@@ -1,12 +1,18 @@
 package com.example.rohit.hack;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,9 +20,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
-import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -24,33 +36,103 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-/*class AmenitiesPost {
-    String heading,description,address,pid;
-    Double lat,lon;
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 
-    AmenitiesPost() {
-
-    }
-    AmenitiesPost(String h,String d,String a) {
-        heading = h;
-        description = d;
-        address = a;
-    } */
-
-
-//}
 
 public class FormFragment extends Fragment {
 
     DatabaseReference db;
 
+    Post post = new Post();
+
+    private LocationRequest mLocationRequest;
+
+    public void onLocationChanged(Location location) {
+        // New location has now been determined
+        post.lat = location.getLatitude();
+        post.lon = location.getLongitude();
+        Geocoder geocoder = new Geocoder(getContext());
+        List<Address> addresses = new ArrayList<>();
+        try {
+            addresses = geocoder.getFromLocation(post.lat,post.lon,1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        post.address = addresses.get(0).getAddressLine(0);
+
+    }
+
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.form_fragment_layout,null);
+        View rootView = inflater.inflate(R.layout.form_fragment_layout, null);
 
         TextView formName = (TextView) rootView.findViewById(R.id.form_name);
+
+        final LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+
+
+        final RadioButton useCurrentLocation = (RadioButton) rootView.findViewById(R.id.current_location_radiobutton);
+
+        final EditText address = (EditText) rootView.findViewById(R.id.amenities_form_address);
+
+
+        useCurrentLocation.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onClick(View view) {
+                if(useCurrentLocation.isChecked() && address.getEditableText().toString().equals("")) {
+                    mLocationRequest = new LocationRequest();
+                    mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+
+                    // Create LocationSettingsRequest object using location request
+                    LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+                    builder.addLocationRequest(mLocationRequest);
+                    LocationSettingsRequest locationSettingsRequest = builder.build();
+
+                    // Check whether location settings are satisfied
+                    // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
+                    SettingsClient settingsClient = LocationServices.getSettingsClient(getActivity());
+                    settingsClient.checkLocationSettings(locationSettingsRequest);
+
+                    if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        ActivityCompat.requestPermissions(getActivity(),
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                99);
+                        return;
+                    }
+                    getFusedLocationProviderClient(getActivity()).requestLocationUpdates(mLocationRequest, new LocationCallback() {
+                                @Override
+                                public void onLocationResult(LocationResult locationResult) {
+                                    // do work here
+                                    onLocationChanged(locationResult.getLastLocation());
+                                    address.setText(post.address);
+                                }
+                            },
+                            Looper.myLooper());
+
+                }
+                else {
+                    useCurrentLocation.setChecked(false);
+                    address.setText("");
+                }
+
+            }
+        });
+
+
 
         if(ProfileActivity.fabOptionClicked == 1) {
             formName.setText("Amenities");
@@ -71,7 +153,7 @@ public class FormFragment extends Fragment {
 
 
         final EditText description = (EditText) rootView.findViewById(R.id.amenities_form_description);
-        final EditText address = (EditText) rootView.findViewById(R.id.amenities_form_address);
+
 
         final EditText heading = (EditText) rootView.findViewById(R.id.amenities_form_heading);
         Button submit = rootView.findViewById(R.id.submit_button);
@@ -80,12 +162,8 @@ public class FormFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
-                Post post = new Post();
                 post.heading = heading.getEditableText().toString();
                 post.description = description.getEditableText().toString();
-                post.address = address.getEditableText().toString();
-
-                //AmenitiesPost amenitiesPost = new AmenitiesPost(heading.getEditableText().toString(),description.getEditableText().toString(),address.getEditableText().toString());
 
                 Geocoder geocoder = new Geocoder(getContext());
                 List<Address> addresses = new ArrayList<>();
